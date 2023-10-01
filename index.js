3aceeb04b73bfc993e41527239abaf7df16d4b6b
Ewerton Shoiti Okada <105746487+ewerton139@users.noreply.github.com>
2023-09-30 23:33:03 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,55 @@
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Solicitar e armazenar o nome do herói fornecido pelo usuário
+        Console.Write("Digite o nome do herói: ");
+        string nome = Console.ReadLine();
+
+        // Solicitar e armazenar a quantidade de experiência (XP) fornecida pelo usuário
+        Console.Write("Digite a quantidade de XP do herói: ");
+        int xp = int.Parse(Console.ReadLine());
+
+        // Estrutura de decisão para determinar o nível do herói com base na quantidade de XP
+        string nivel;
+
+        if (xp < 1000)
+        {
+            nivel = "Ferro";
+        }
+        else if (xp >= 1001 && xp <= 2000)
+        {
+            nivel = "Bronze";
+        }
+        else if (xp >= 2001 && xp <= 5000)
+        {
+            nivel = "Prata";
+        }
+        else if (xp >= 6001 && xp <= 7000)
+        {
+            nivel = "Ouro";
+        }
+        else if (xp >= 7001 && xp <= 8000)
+        {
+            nivel = "Platina";
+        }
+        else if (xp >= 8001 && xp <= 9000)
+        {
+            nivel = "Ascendente";
+        }
+        else if (xp >= 9001 && xp <= 10000)
+        {
+            nivel = "Imortal";
+        }
+        else
+        {
+            nivel = "Radiante";
+        }
+
+        // Exibir a mensagem com o nome e o nível do herói
+        Console.WriteLine($"O Herói de nome {nome} está no nível de {nivel}");
+
+        // Aguardar a entrada do usuário antes de fechar o programa
+        Console.ReadLine();
+    }
+}
