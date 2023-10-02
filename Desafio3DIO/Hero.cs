cd869a3469682cccf6c122924928181a3e3e8308
Ewerton Shoiti Okada <105746487+ewerton139@users.noreply.github.com>
2023-10-01 22:54:38 -0300

    Desafio 3 DIO
    Desafio de classe e objeto herói.

---
+++ Desafio3DIO/Hero.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio3DIO
+{
+    public class Hero
+    {
+        public string nome { get; set; }
+        public int idade { get; set; }
+        public string tipo { get; set; }
+
+        public Hero(string nome, int idade, string tipo)
+        {
+            this.nome = nome;
+            this.idade = idade;
+            this.tipo = tipo;
+        }
+
+        public void Atacar(string tipo)
+        {
+            string ataque = "";
+
+            if (tipo == "guerreiro")
+            {
+                ataque = "usando espada";
+            }
+            else if (tipo == "mago")
+            {
+                ataque = "usando magia";
+            }
+            else if (tipo == "monge")
+            {
+                ataque = "usando artes marciais";
+            }
+            else if (tipo == "ninja")
+            {
+                ataque = "usando shuriken";
+            }
+            Console.WriteLine($"O {tipo} atacou {ataque}\n");
+        }
+    }
+}
---
+++ Desafio3DIO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio3DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero guerreiro = new Hero("Yuusha", 20, "guerreiro");
+            guerreiro.Atacar(guerreiro.tipo);
+
+            Hero mago = new Hero("Yuusha", 20, "mago");
+            mago.Atacar(mago.tipo);
+
+            Hero monge = new Hero("Yuusha", 20, "monge");
+            guerreiro.Atacar(monge.tipo);
+
+            Hero ninja = new Hero("Yuusha", 20, "ninja");
+            ninja.Atacar(ninja.tipo);
+
+            Console.ReadLine();
+        }
+    }
+}
